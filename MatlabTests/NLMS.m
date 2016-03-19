36dc7d41185809721acecfd288d9ea98031d3ecc
David <moreoverlords@gmail.com>
2016-03-19 14:02:33 -0400

    displays anti-noise, proper normalization, dreams

--- MatlabTests/NLMS.m
+++ MatlabTests/NLMS.m
@@ -1,13 +1,14 @@
 function [ output, error ] = NLMS( waveform, desired, stepsize )
-%NLMS: Normalized Least Means Squared
-%   
+% NLMS: Normalized Least Means Squared
+
     L = length(waveform);
-    output = zeros(L);
-    error = zeros(L);
+    output = zeros(1, L);
+    error = zeros(1, L);
     
     for i = 2:L
-        error[i-1] = desired[i-1] - output[i-1];
-        output[i] = output[i-1] + (stepsize * error[i-1] * waveform[i-1])/(waveform[i-1] * waveform[i-1]);
+        error(i-1) = desired(i-1) - output(i-1);
+        output(i) = output(i-1) + (stepsize * error(i-1) * waveform(i-1))/(waveform(i-1) * waveform(i-1));
     end
+    
 end
 
--- MatlabTests/lms.m
+++ MatlabTests/lms.m
@@ -3,7 +3,7 @@
 clear
 close all
 
-order=2;
+order=5;
 
 size=2;                         %time duration of inputs
 fs=44100;                                %digital sampling frequency
@@ -13,35 +13,50 @@ f1=440;                                %frequency of voice
 f2=500;                                %frequency of noise
 
 voice=cos(2*pi*f1*t);
-subplot(4,1,1)
+subplot(5,1,1)
 plot(t,voice);
 title('voice    (don''t have access to)')
 audiowrite('voice.wav', voice, fs);
 
-noise=cos(2*pi*f2*t.^2);                       %frequency sweep noise
-%noise=.1*(rand(1,length(voice))-.5);            %white noise
-primary=voice+noise;
-subplot(4,1,2)
-plot(t,primary)
-title('primary = voice + noise   (input1)')
-audiowrite('primary.wav', primary, fs);
+%noise=cos(2*pi*f2*t.^2);                       %frequency sweep noise
+noise=(rand(1,length(voice))-.5);            %white noise
+%noise=cos(2*pi*f2*t.^2)+cos(2*pi*t*((f2+200)/t));                       %2-frequency sweeps noise
+input=voice+noise;
+subplot(5,1,2)
+plot(t,input)
+title('input = voice + noise   (input1)')
+audiowrite('input.wav', input, fs);
 
-ref=noise+.25*(rand-0.5);                       %noisy noise
-subplot(4,1,3)
+ref=noise +.25*(rand-0.5);                       %noisy noise
+subplot(5,1,3)
 plot(t,ref)
 title('reference  (noisy noise)   (input2)');
 audiowrite('reference.wav', ref, fs);
 
 w=zeros(order,1);
+error = zeros(N-order, 1);
+antinoise = zeros(N-order, 1);
+delay = 0;
 mu=.006;
 for i=1:N-order
-   buffer = ref(i:i+order-1);                   %current 32 points of reference
-   desired(i) = primary(i)-buffer*w;            %dot product reference and coeffs
-   w=w+(buffer.*mu*desired(i)/norm(buffer))';   %update coeffs
+   buffer = ref(i:i+order-1);                  
+   antinoise(i) = buffer*w;
+   if i > delay
+        error(i) = input(i)-antinoise(i-delay);      
+   else 
+        error(i) = input(i)-antinoise(i);            
+   end
+   w=w+(buffer.*mu*error(i)/(buffer*buffer'))';  
 end
 
-subplot(4,1,4)
-plot(t(order+1:N),desired)
+subplot(5,1,4)
+plot(t(order+1:N),antinoise)
+title('Anti-noise (added to isolate voice)')
+
+audiowrite('antinoise.wav', antinoise, fs);
+
+subplot(5,1,5)
+plot(t(order+1:N),error)
 title('Adaptive output  (hopefully it''s close to "voice")')
 
-audiowrite('desired.wav', desired, fs);
+audiowrite('error.wav', error, fs);
